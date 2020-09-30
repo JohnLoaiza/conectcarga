2d887da352d50844fd0ed0d0315284ea9e271570
John <jhon91811@gmail.com>
2020-09-29 21:11:13 -0500

    play store v2

--- build/app/generated/source/buildConfig/release/net/citica/conectcarga/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/net/citica/conectcarga/BuildConfig.java
@@ -5,9 +5,8 @@ package net.citica.conectcarga;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "net.citica.conectcarga";
+  public static final String APPLICATION_ID = "net.citica.conectcargav2";
   public static final String BUILD_TYPE = "release";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
 }
